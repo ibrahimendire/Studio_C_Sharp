ecd27b40d688f0b4a03e63716848735a954e7149
ibrahimendire <ibrahimendire@users.noreply.github.com>
2020-09-28 17:03:12 -0500

    initial Commit

---
+++ Studio_Area_of_a_Circle/Circle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Studio_Area_of_a_Circle
+{
+    public class Circle
+    {
+        public Circle()
+        {
+        }
+    }
+}
---
+++ Studio_Area_of_a_Circle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Studio_Area_of_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a radius: ");
+            string userInput = Console.ReadLine();
+            double radius = double.Parse(userInput);
+          
+            double area = radius * radius * Math.PI;
+            double circumference = 2 * radius * Math.PI;
+            if(radius >= 0)
+            {
+                Console.WriteLine("The area of a circle of radius " + userInput + " is : " + area);
+                Console.WriteLine("The circumference of a circle of radius " + userInput + " is : " + circumference + "\n");
+
+                Console.Write("Enter miles per gallon of your car :");
+                userInput = Console.ReadLine();
+                double milePerGallon = double.Parse(userInput);
+                if (milePerGallon > 0)
+                {
+                    Console.WriteLine("you  will use " + radius * (1 / milePerGallon) + " gallon of gas to go around this circle.");
+                }else { Console.Write("Please input Valid data (mile per gallon of your Car gas consumtion) "); }
+            }
+            else { Console.WriteLine("Please enter valid input"); }
+
+            
+        }
+    }
+}
